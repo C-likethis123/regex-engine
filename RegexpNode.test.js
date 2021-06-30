f9ade68381115143d7b691b4d533cc9362255595
Chow Jia Ying <chowjiaying211@gmail.com>
2021-06-30 23:07:34 +0800

    add unit test cases for RegexpNode

--- RegexpNode.test.js
+++ RegexpNode.test.js
@@ -1,5 +1,16 @@
 const { RegexpNode, NeverMatches } = require("./RegexpNode");
+
 test('Derivative of a RegexpNode does not match anything', () => {
   const regexNode = new RegexpNode();
   expect(regexNode.derive() === NeverMatches).toBe(true); 
+});
+
+test('A RegexpNode does not match the end', () => {
+  const regexNode = new RegexpNode();
+  expect(regexNode.matchEnd()).toBe(false);
+});
+
+test('A RegexpNode can always match more nodes', () => {
+  const regexNode = new RegexpNode();
+  expect(regexNode.canMatchMore()).toBe(true);
 })
