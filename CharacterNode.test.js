7858daec32835b7744c46dd275985ee6c6338ffc
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-06 23:45:41 +0800

    test: CharacterNode unit test

---
+++ CharacterNode.test.js
@@ -0,0 +1,13 @@
+const {CharacterNode, EmptyString, NeverMatches} = require("./RegexpNode.js");
+
+test('Progresses to the next character when deriving', () => {
+  const expectedDerivedSequence = new CharacterNode('e', EmptyString);
+  const testCharacterNode = new CharacterNode('d', expectedDerivedSequence);
+  expect(testCharacterNode.derive('d')).toBe(expectedDerivedSequence);
+});
+
+test('Returns NeverMatch if current character node does not match derivative input', () => {
+  const expectedDerivedSequence = new CharacterNode('e', EmptyString);
+  const testCharacterNode = new CharacterNode('d', expectedDerivedSequence);
+  expect(testCharacterNode.derive('e')).toBe(NeverMatches);
+});
