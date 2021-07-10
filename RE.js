35f2395522b7eb712a054cd11c81c9fe3a73dacf
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-11 01:27:01 +0800

    refactor: move RE class into its own file

---
+++ RE.js
@@ -0,0 +1,32 @@
+const {compile} = require("./Compiler.js");
+const {EmptyString} = require("./RegexpNode.js");
+const {Flags} = require("./Flag.js");
+
+/**
+ * Entry point for the compiler
+ */
+class RE {
+  constructor(regexp, flags = []) {
+    this.start = compile(regexp);
+  }
+
+  match(str) {
+    let state = this.start;
+    const chars = Array.from(str);
+    if ((chars.length === 0) && (state === EmptyString)) {
+      return true;
+    }
+    for (let i = 0; i < chars.length; i++) {
+      let char = chars[i];
+      state = state.derive(char);
+      if (state.matchEnd() && i === chars.length - 1) {
+        return true;
+      } else if (state.matchEnd() && !state.canMatchMore()) {
+        return false;
+      }
+    }
+    return false;
+  }
+}
+module.exports = {RE};
+
