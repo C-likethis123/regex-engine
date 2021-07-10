f725721e1233dcd1bf3d0a2ab0edddc2a50e1662
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-11 01:04:08 +0800

    feat: add case insensitive compilation mode

--- Compiler.js
+++ Compiler.js
@@ -5,31 +5,47 @@ const {_Or, _ZeroOrMore, Any} = require('./StringAST.js');
  * The entry point for the compiler. It takes in literal strings and turns them into chains of character nodes.
  */
 
-function compileString(str, tail = EmptyString) {
+function isAlphabet(char) {
+  const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabets.includes(char);
+}
+
+function reverseCase(char) {
+  if (char === char.toUpperCase()) {
+    return char.toLowerCase();
+  }
+  return char.toUpperCase();
+}
+
+function compileString(str, tail = EmptyString, isCaseInsensitive = false) {
   let reversedStr = Array.from(str).reverse();
   for (const char of reversedStr) {
-    tail = new CharacterNode(char, tail);
+    if (isAlphabet(char) && isCaseInsensitive) {
+      tail = new AlternationNode([new CharacterNode(char, tail), new CharacterNode(reverseCase(char), tail)]);
+    } else {
+      tail = new CharacterNode(char, tail);
+    }
   }
   return tail;
 }
 
-function compileArray(arr, tail = EmptyString) {
+function compileArray(arr, tail = EmptyString, isCaseInsensitive = false) {
   for (let expr of arr.reverse()) {
-    tail = compile(expr, tail);
+    tail = compile(expr, tail, isCaseInsensitive);
   }
   return tail;
 }
 
-function compileOr(or, tail = EmptyString) {
+function compileOr(or, tail = EmptyString, isCaseInsensitive = false) {
   const compiledAlternatives = or.alternatives.map(
-    alternative => compile(alternative, tail)
+    alternative => compile(alternative, tail, isCaseInsensitive)
   );
   return new AlternationNode(compiledAlternatives);
 }
 
-function compileZeroOrMore(zeroOrMore, tail = EmptyString) {
+function compileZeroOrMore(zeroOrMore, tail = EmptyString, isCaseInsensitive = false) {
   const repetition = new RepetitionNode(tail);
-  const contents = compile(zeroOrMore.repeatable, repetition);
+  const contents = compile(zeroOrMore.repeatable, repetition, isCaseInsensitive);
   repetition.head = contents;
   return repetition;
 }
@@ -38,15 +54,15 @@ function compileAny(tail = EmptyString) {
   return new AnyCharacterNode(tail);
 }
 
-function compile(expr, tail = EmptyString) {
+function compile(expr, tail = EmptyString, isCaseInsensitive = false) {
   if ((typeof expr) === 'string') {
-    return compileString(expr, tail);
+    return compileString(expr, tail, isCaseInsensitive);
   } else if (expr instanceof Array) {
-    return compileArray(expr, tail);
+    return compileArray(expr, tail, isCaseInsensitive);
   } else if (expr instanceof _Or) {
-    return compileOr(expr, tail);
+    return compileOr(expr, tail, isCaseInsensitive);
   } else if (expr instanceof _ZeroOrMore) {
-    return compileZeroOrMore(expr, tail);
+    return compileZeroOrMore(expr, tail, isCaseInsensitive);
   } else if (expr === Any) {
     return compileAny(tail);
   } else {
