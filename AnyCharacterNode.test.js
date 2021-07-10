e34b639a5248dbb93a6083e1d28c0885907ad4b2
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-11 00:03:28 +0800

    test: add unit tests for AnyCharacter

---
+++ AnyCharacterNode.test.js
@@ -0,0 +1,10 @@
+const {AnyCharacterNode, EmptyString, NeverMatches} = require('./RegexpNode.js');
+
+test('Matches all alphanumeric characters', () => {
+  const alphanumericString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (const letter of alphanumericString) {
+    const testAnyCharacterNode = new AnyCharacterNode(EmptyString);
+    expect(testAnyCharacterNode.derive(letter)).toBe(EmptyString);
+  }
+});
+
