0ce08a2ec3f1178ca92f42d2d968ad0b1c01298d
Chow Jia Ying <chowjiaying211@gmail.com>
2021-07-20 13:03:37 +0800

    test: character groups are parsed correctly

--- Grammer/Parser.test.js
+++ Grammer/Parser.test.js
@@ -3,3 +3,10 @@ const parser = require('./Parser.js');
 test('Parses regex of only characters into an array of character elements', () => {
   expect(parser.parse("abc")).toStrictEqual(["a", "b", "c"]);
 });
+
+test('Parses regex of character groups into arrays of letters', () => {
+  expect(parser.parse("(abc)")).toStrictEqual([["a", "b", "c"]]);
+  expect(parser.parse("(ab)c")).toStrictEqual([["a", "b"], "c"]);
+  expect(parser.parse("a(bc)(cd)")).toStrictEqual(["a", ["b", "c"], ["c", "d"]]);
+});
+
